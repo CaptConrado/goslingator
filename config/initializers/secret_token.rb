9f00630f7e661597ca69734683abf58d1a7acc7e
CaptConrado <ottey001@gmail.com>
2013-10-15 19:22:31 -0700

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ryaninator::Application.config.secret_token = 'c8475f69ceb3486f987866a707901b08a64e269d64b1bbd9f735dffb6bd225c8ba02a1c80aea2f519cea1c1c18831fe33d43dd2781b2338e03b5b2db9580b141'
