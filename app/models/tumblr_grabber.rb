612d59f908618ab2364c93bdb7052fa611c2078c
CaptConrado <ottey001@gmail.com>
2013-10-15 20:44:13 -0700

    cleaned up

--- app/models/tumblr_grabber.rb
+++ app/models/tumblr_grabber.rb
@@ -3,16 +3,11 @@ class TumblrGrabber < ActiveRecord::Base
   attr_accessible :url, :vote
 
   @url = "http://api.tumblr.com/v2/tagged?tag=ryan_gosling&api_key=fuiKNFp9vQFvjLNvx4sUwti4Yb5yGutBN4Xh10LXZhhRKjWlV4"
-  # @response = ActiveSupport::JSON.decode(@url)
   @response = `curl "http://api.tumblr.com/v2/tagged?tag=ryan_gosling&api_key=fuiKNFp9vQFvjLNvx4sUwti4Yb5yGutBN4Xh10LXZhhRKjWlV4"`
-
   @response = JSON.parse(@response)
-  debugger
+
   picture1 = @response['response']['photos']['url']
   picture1.regex('"url\\":\\"(.+?)"')
-  debugger
-  # picture1.gsub('/\','')
-  puts picture1
 
 
 end
